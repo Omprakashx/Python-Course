2165c513274f218e727422b0ef0eb54990f38f1a
Om Singh <86137170+Omprakashx@users.noreply.github.com>
2023-06-05 17:44:05 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,25 @@
+new=["ashish", "om", "shivam", "varun","anurag"]
+print(new)
+print("hello duniya")
+print(new[2])
+
+num=[2,3,8,1,9,5,4,7]
+
+num.sort()
+print(num)
+num.reverse()
+print(num)
+num.insert(3,0)
+print(num)
+
+num.append(12)
+print(num)
+
+num.remove(12)
+print(num)
+
+num.pop(3)
+print(num)
+
+print(num[0,5])
+
